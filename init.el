68f1874772b3f2036483ad352965b13331e05578
wangkaichao <wangkaichao@skyworth.com>
2019-03-27 08:54:34 +0800

    display line number
    Change-Id: I35080b3a6e1e843634fc6d108c73da3c484a97b5

--- init.el
+++ init.el
@@ -3,3 +3,21 @@
 
 (setq make-backup-files nil)
 (put 'narrow-to-page 'disabled nil)
+
+;(global-set-key [C-M-S-v] 'scroll-other-window-down)
+
+(defun scroll-other-window-up ()
+  "Scroll the other window one line up."
+  (interactive)
+  (scroll-other-window -1)
+)
+(defun scroll-other-window-down ()
+  "Scroll the other window one line down."
+  (interactive)
+  (scroll-other-window 1)
+)
+(global-set-key [C-M-S-up] 'scroll-other-window-up)
+(global-set-key [C-M-S-down] 'scroll-other-window-down)
+
+(global-linum-mode t)
+(setq linum-format "%4d |")
