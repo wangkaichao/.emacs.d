a94c499c974559158945f915b274762fe5e808be
wangkaichao <364596530@qq.com>
2020-03-12 12:57:52 +0800

    modify ycmd_cpp.el

--- auto-save-list/ycmd_cpp.el
+++ auto-save-list/ycmd_cpp.el
@@ -1,18 +1,18 @@
 ;;;;================================================= < for ycmd start >
 ;;;; To use ycmd-mode in all supported modes
-;(require 'ycmd)
+(require 'ycmd)
 ;(add-hook 'after-init-hook #'global-ycmd-mode)
 
 ;;;; Specify only support c/c++ mode
-(require 'ycmd)
 (add-hook 'c++-mode-hook 'company-mode)
 (add-hook 'c++-mode-hook 'ycmd-mode)
 
 ;;;; Specify how to run the server
-(set-variable 'ycmd-server-command '("/usr/local/bin/python3" "/Users/wangkaichao/work/3rd/ycmd/ycmd"))
+(set-variable 'ycmd-server-command '("/usr/bin/python3" "/opt/source/3rd/ycmd/ycmd"))
 
 ;;;; Specify a global emacs configuration
-(set-variable 'ycmd-global-config "/Users/wangkaichao/work/3rd/ycmd/examples/.ycm_extra_conf.py")
+(set-variable 'ycmd-global-config "/opt/source/3rd/ycmd/ycmd/examples/.ycm_extra_conf.py")
+(set-variable 'ycmd-extra-conf-whitelist '("/opt/source/work/wkc01/project/*"))
 
 ;;;; Completion framework
 (require 'company-ycmd)
@@ -20,9 +20,9 @@
 (add-hook 'after-init-hook #'global-company-mode)  
 
 ;;;; Enable flycheck
-(require 'flycheck-ycmd)
-(flycheck-ycmd-setup)
-(add-hook 'after-init-hook #'global-flycheck-mode)  
+;(require 'flycheck-ycmd)
+;(flycheck-ycmd-setup)
+;(add-hook 'after-init-hook #'global-flycheck-mode)  
 
 ;;;; Set always complete immediately
 (setq company-idle-delay 0)
